721ac8f052034092b495efa518736c07b007f1f7
Paulo Marcelino <82913040+PauloHennry@users.noreply.github.com>
2022-01-23 03:08:41 +0000

    Filter rsrsr

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/*
-As maças custam R$0,30 cada se forem compradas menos o que 1 dúzia, e R$ 0,25 se forem compradas pelo menos 12. 
-
-Escreva um programa que leia o numero  de maças compradas , calcule e escreva o valor total.
-
-divisão e problemas:
-
-	- preço das maças R$0.30 equivalente a menos de 1 duzia
-	- preço 0.25 se for comprado 1 duzia
-
- */
-
 const PRECO_MENOS_DUZIA = 0.30;
 const PRECO_DUZIA = 0.25;
 
